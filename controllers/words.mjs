c1c714a8b7f6648cfdf495b16d540d411b750e38
Katie Nink Smith <katiej.smith@outlook.com>
2025-01-17 11:31:56 -0700

    getWords, userWords, findByCategory, and createWord routes all working on the backend

--- controllers/words.mjs
+++ controllers/words.mjs
@@ -1,36 +1,37 @@
 import Words from "../models/words.mjs";
-import multer from "multer";
-const upload = multer({ dest: "uploads/" });
+// import multer from "multer";
+// const upload = multer({ dest: "uploads/" });
 
-// get all words for the user
+// get all words
 async function getWords(req, res) {
   try {
-    const foundWords = await Words.find({});
+    const foundWords = await Words.find();
     res.status(200).json(foundWords);
   } catch (err) {
     res.status(400).send(err);
   }
 }
 
-// get a single word for the user
-async function getWord(req, res) {
+// get words for one user
+async function userWords(req, res) {
+  console.log(req.params);
   try {
-    const foundWord = await Words.findById(req.params.id);
-    res.status(200).json(foundWord);
+    const foundWords = await Words.find({ user: req.params.user });
+    res.status(200).json(foundWords);
   } catch (err) {
     res.status(400).send(err);
   }
 }
 
 // get all words from a category for the user
-async function category(req, res) {
+async function findByCategory(req, res) {
+  console.log(req.params, "find by category");
   try {
-    const foundCategory = await Words.find().all("category", [
-      "family",
-      "places",
-      "things",
-      "clothing",
-    ]);
+    const foundCategory = await Words.find({
+      user: req.params.user,
+      category: req.params.category,
+    });
+    // .all({ category: req.params.category,});
     res.status(200).json(foundCategory);
   } catch (err) {
     res.status(400).send(err);
@@ -40,17 +41,30 @@ async function category(req, res) {
 // allow user to create a word
 async function createWord(req, res) {
   try {
-    console.log(req.body);
-    const createdWord = await Words.create(req.body);
-    console.log(createdWord);
-    res.status(200).json(createdWord);
+    console.log(req.body, "sent");
+    // const { word, category, user } = req.body; // Form data
+    // console.log(req.body.word, req.body.category, req.body.user, "sent info");
+    const createdWord = new Words(req.body);
+    console.log(createdWord, "next step");
+    createdWord.save();
+    console.log(createdWord, "new");
+    res.status(200).json({ message: "Word and files uploaded successfully" });
   } catch (err) {
+    console.log("its not adding");
     res.status(400).send(err);
   }
 }
 
 // allow a user to edit their created word
 async function editWord(req, res) {
+  console.log(req.params, "url id");
+  console.log(req.body, "why you forget");
+
+  // Basic validation (optional, depending on your requirements)
+  if (!req.body || Object.keys(req.body).length === 0) {
+    return res.status(400).json({ message: "No data provided for update" });
+  }
+
   try {
     const updatedWord = await Words.findByIdAndUpdate(req.params.id, req.body, {
       new: true,
@@ -61,21 +75,21 @@ async function editWord(req, res) {
   }
 }
 
-// allow user to delete their created word
-async function deleteWord(req, res) {
-  try {
-    const deletedWord = await Words.findByIdAndDelete(req.params.id);
-    res.status(200).json(deletedWord);
-  } catch (err) {
-    res.status(400).send(err);
-  }
-}
+// // allow user to delete their created word
+// async function deleteWord(req, res) {
+//   try {
+//     const deletedWord = await Words.findByIdAndDelete(req.params.id);
+//     res.status(200).json(deletedWord);
+//   } catch (err) {
+//     res.status(400).send(err);
+//   }
+// }
 
 export default {
   getWords,
-  getWord,
-  category,
+  userWords,
+  findByCategory,
   createWord,
   editWord,
-  deleteWord,
+  // deleteWord,
 };
--- routes/words.mjs
+++ routes/words.mjs
@@ -2,42 +2,27 @@
 // ** imports
 import express from "express";
 const router = express.Router();
-import userController from "../controllers/words.mjs";
-import multer from "multer";
-const upload = multer({ dest: "uploads/" });
+import wordsController from "../controllers/words.mjs";
+// import multer from "multer";
+// const upload = multer({ dest: "uploads/" });
 import Words from "../models/words.mjs";
 
-// // this corresponds to get all user's words on the frontend
-// router.get("/:id/words", userController.getWords);
+// this corresponds to get all user's words on the frontend
+router.get("/", wordsController.getWords);
 
-// // this corresponds to get a user's word on the frontend
-// router.get("/:id/:word", userController.getWord);
+// this corresponds to get a user's word on the frontend
+router.get("/:user", wordsController.userWords);
 
-// // this corresponds to a category of user's words on the frontend
-// router.get("/:id/words/:category", userController.category);
+// this corresponds to user's word in a category
+router.get("/:user/:category", wordsController.findByCategory);
 
-// this corresponds to create a user's word in the front end
-// router.post("/upload", userController.createWord);
-router.post("/", async (req, res) => {
-  try {
-    console.log(req.body, "sent");
-    // const { word, category, user } = req.body; // Form data
-    // console.log(req.body.word, req.body.category, req.body.user, "sent info");
-    const createdWord = new Words(req.body);
-    console.log(createdWord, "next step");
-    createdWord.save();
-    console.log(createdWord, "new");
-    res.status(200).json({ message: "Word and files uploaded successfully" });
-  } catch (err) {
-    console.log("its not adding");
-    res.status(400).send(err);
-  }
-});
+// this corresponds to create a user's word on the frontend
+router.post("/", wordsController.createWord);
 
-// // this corresponds to edit a user's word on the frontend
-// router.put("/:id/:word", userController.editWord);
+// this corresponds to edit a user's word on the frontend
+router.put("/:id/edit", wordsController.editWord);
 
 // // this corresponds to delete a user's word on the frontend
-// router.delete("/:id/:word", userController.deleteWord);
+// router.delete("/:id/:word", wordsController.deleteWord);
 
 export default router;
